83eaa71bd9d73e74e14a7cf6245d1f11a699a767
matheus <matheusperotto@outlook.com>
2026-02-18 00:15:37 -0800

    remover arquivo auxiliar desnecessario

--- sync_git.ps1
+++
@@ -1,5 +0,0 @@
-Set-Location "c:\Users\pagglo\Documents\Projeto Nox Roguelike\Projeto Nox Roguelike\Projeto Nox Roguelike\OneDrive\Desktop\Projeto Kodland"
-$env:GIT_EDITOR = "true"
-git config core.editor true
-git pull --rebase origin main
-git push origin main
