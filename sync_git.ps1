5211927a1e2cc66998f35f9d6dfb3526f8e50e31
matheus <matheusperotto@outlook.com>
2026-02-18 00:12:00 -0800

    ajustes finais

---
+++ sync_git.ps1
@@ -0,0 +1,5 @@
+Set-Location "c:\Users\pagglo\Documents\Projeto Nox Roguelike\Projeto Nox Roguelike\Projeto Nox Roguelike\OneDrive\Desktop\Projeto Kodland"
+$env:GIT_EDITOR = "true"
+git config core.editor true
+git pull --rebase origin main
+git push origin main
